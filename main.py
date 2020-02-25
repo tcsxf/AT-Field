8a2f3b032e8f1cb6b7651b1aa140a6d2722ba82d
shenxufeng <450034147@qq.com>
2020-02-25 21:51:33 +0800

    改用sha算法

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
 from secret import *
 
-
-tp = input('模式:1高级加密 2简单加密 3批量加密\n')
-act = input('动作:1加密 2解密\n')
-key = input('输入秘钥\n')
+tp = input('模式:1高级加密 2简单加密 3批量加密 4获取信息\n')
+inf = input('文件\\目录名称:\n')
+if tp in ('1', '2', '3'):
+    act = input('动作:1加密 2解密\n')
+    key = input('输入秘钥\n')
 if tp == '1':
-    infile = input('原文件名:\n')
-    outfile = input('输出文件名:\n')
+    outf = input('输出文件名:\n')
     if act == '1':
-        key_if = input('输入任意字符秘钥写入\n')
         name_if = input('输入任意字符文件名写入\n')
-        if encrypt(infile, outfile, key, key_if, name_if):
+        key_if = input('输入任意字符秘钥写入\n')
+        if encrypt(inf, outf, key, name_if, key_if):
             print('加密成功')
     elif act == '2':
-        if decrypt(infile, outfile, key):
+        if decrypt(inf, outf, key):
             print('解密成功')
 elif tp == '2':
-    infile = input('原文件名:\n')
     if act == '1':
-        if enbiter(infile, key):
+        if ezbiter(inf, key):
             print('加密成功')
     elif act == '2':
-        if debiter(infile, key):
+        if ezbiter(inf, key):
             print('解密成功')
 elif tp == '3':
     rec = input('输入任意字符递归操作子文件夹\n')
     if act == '1':
-        if batch_enbiter(key, rec=rec):
+        if batch_biter(key, inf, rec):
             print('批量加密成功')
     elif act == '2':
-        if batch_debiter(key, rec=rec):
+        if batch_debiter(key, inf, rec):
             print('批量解密成功')
+elif tp == '4':
+    get_key(inf)
 else:
     print('操作失败')
--- secret.py
+++ secret.py
@@ -1,118 +1,93 @@
+import hashlib
+import random
 import os
 import json
 
 
-sxf_key = [
-    'Saber',
-    'Lancer',
-    'Archer',
-    'Rider',
-    'Caster',
-    'Assassin',
-    'Berserker',
-    'Shielder'
-]
-
-sxf_value = [
-    'Archer',
-    'Saber',
-    'Lancer',
-    'Assassin',
-    'Rider',
-    'Caster',
-    'Shielder',
-    'Berserker'
-]
+def xor(byte, key, flag=32):
+    # byte: 输入字节流
+    # key: 加密关键字
+    # flag: 取的散列值字节数
+    # 函数对key散列到256位，取其中的flag个字节，位数扩充到>=输入的位数，两者进行异或
+    sha = hashlib.sha256()
+    sha.update(key.encode('utf-8'))
+    code = sha.digest()[:flag]
+    length = len(byte) // flag + 1
+    code = code * length
+    ret = map(lambda x: x[0] ^ x[1], zip(byte, code))
+    return bytes(ret)
 
 
-def byte_lock(byte, flag=7):
-    # 对字节流按flag位进行亦或操作
-    byte_lock = map(lambda x: x ^ flag, byte)
-    return bytes(byte_lock)
-
-
-def encrypt(infile, outfile, key='Saber', key_if=False, name_if=False):
+def encrypt(infile, outfile, key='Saber', nm_if=False, key_if=False):
     # 对全文件的字节流按key进行加密,并在文件头中写入相关信息
-    if key not in sxf_key:
-        return False
     with open(outfile, 'wb') as outf:
+        if nm_if:
+            buff = os.path.basename(infile).encode()
+            outf.write(chr(len(buff)).encode())
+            outf.write(bytes(map(lambda x: x ^ 255, buff)))
+        else:
+            outf.write(bytes([0]))
         if key_if:
-            value = sxf_value[sxf_key.index(key)]
-            outf.write(chr(len(value)).encode())
-            buff = value.encode()
-            outf.write(byte_lock(buff))
+            buff = key.encode()
+            outf.write(chr(len(buff)).encode())
+            outf.write(bytes(map(lambda x: x ^ 255, buff)))
         else:
             outf.write(bytes([0]))
-
-        if name_if:
-            outf.write(chr(len(infile)).encode())
-            buff = infile.encode()
-            outf.write(byte_lock(buff))
-        else:
-            outf.write(bytes([0]))
-
-        flag = 1 << sxf_key.index(key)
+        flag = random.randint(17, 32)
+        outf.write(chr(flag).encode())
         with open(infile, 'rb') as inf:
             buff = inf.read(1024)
             while buff:
-                outf.write(byte_lock(buff, flag))
+                outf.write(xor(buff, key, flag))
                 buff = inf.read(1024)
     return True
 
 
-def decrypt(infile, outfile, key='Archer'):
+def decrypt(infile, outfile, key='Saber'):
     # 先读取文件头的相关信息,再根据key对文件进行解密
     with open(infile, 'rb') as inf:
+        nm_len = ord(inf.read(1))
+        inf.read(nm_len)
         key_len = ord(inf.read(1))
-        buff = inf.read(key_len)
-        key_ = byte_lock(buff).decode()
-        if not key:
-            key = key_
-        if key not in sxf_key:
-            return False
+        inf.read(key_len)
 
-        name_len = ord(inf.read(1))
-        buff = inf.read(name_len)
-        outfile_ = byte_lock(buff).decode()
-        if not outfile:
-            outfile = outfile_
-        if not outfile:
-            return False
-
-        flag = 1 << sxf_value.index(key)
+        flag = ord(inf.read(1))
         with open(outfile, 'wb') as outf:
             buff = inf.read(1024)
             while buff:
-                outf.write(byte_lock(buff, flag))
+                outf.write(xor(buff, key, flag))
                 buff = inf.read(1024)
     return True
 
 
-def enbiter(infile, key='Saber'):
+def get_key(infile):
+    with open(infile, 'rb') as inf:
+        nm_len = ord(inf.read(1))
+        nm = inf.read(nm_len)
+        if nm:
+            nm_ = bytes(map(lambda x: x ^ 255, nm)).decode()
+            print(f'原文件名:{nm_}')
+        else:
+            print('无法获取原文件名')
+        key_len = ord(inf.read(1))
+        key = inf.read(key_len)
+        if key:
+            key_ = bytes(map(lambda x: x ^ 255, key)).decode()
+            print(f'KEY:{key_}')
+        else:
+            print('无法获取KEY')
+
+
+def ezbiter(infile, key='Saber'):
     # 对原文件的头部信息按key进行加密,适合大文件
-    if key not in sxf_key:
-        return False
     with open(infile, 'rb+') as inf:
-        flag = 1 << sxf_key.index(key)
-        buff = inf.read((1 + sxf_key.index(key)) << 20)
+        buff = inf.read(1 << 20)
         inf.seek(0)
-        inf.write(byte_lock(buff, flag))
+        inf.write(xor(buff, key))
     return True
 
 
-def debiter(infile, key='Archer'):
-    # 对加密文件头部信息按key进行解密
-    if key not in sxf_key:
-        return False
-    with open(infile, 'rb+') as inf:
-        flag = 1 << sxf_value.index(key)
-        buff = inf.read((1 + sxf_key.index(key)) << 20)
-        inf.seek(0)
-        inf.write(byte_lock(buff, flag))
-    return True
-
-
-def batch_enbiter(key='Saber', path='.', rec=False):
+def batch_biter(key='Saber', path='.', rec=False):
     # 用key批量加密当前文件夹下所有文件,并重命名
     # 把文件对应关系写到0.sxf文件中
     names = {}
@@ -120,18 +95,13 @@ def batch_enbiter(key='Saber', path='.', rec=False):
     for i, n in enumerate(os.listdir(path)):
         fd = os.path.join(path, n)
         # 判断读写权限
-        if not os.access(fd, 6):
+        if not os.access(fd, 6) or fd[-3:] in ('BIN', 'bin', 'sys', '.py', 'pyc', 'dea'):
             continue
         if rec and os.path.isdir(fd):
-            batch_enbiter(key, fd, rec)
+            batch_biter(key, fd, rec)
             continue
-
-        # 判断是否文件
-        can = os.path.isfile(fd)
-        # 剔除指定类型文件
-        can &= fd[-3:] not in ('BIN', 'bin', 'sys', '.py', 'pyc')
-        if can:
-            if enbiter(fd, key):
+        elif os.path.isfile(fd):
+            if ezbiter(fd, key):
                 os.rename(fd, os.path.join(path, str(i + 1) + '.sxf'))
                 names[str(i + 1) + '.sxf'] = n
             else:
@@ -140,12 +110,12 @@ def batch_enbiter(key='Saber', path='.', rec=False):
         ret = True
     if ret:
         with open(os.path.join(path, '0.sxf'), 'wb') as f:
-            buff = byte_lock(json.dumps(names).encode())
+            buff = bytes(map(lambda x: x ^ 255, json.dumps(names).encode()))
             f.write(buff)
     return ret
 
 
-def batch_debiter(key='Archer', path='.', rec=False):
+def batch_debiter(key='Saber', path='.', rec=False):
     # 读取0文件的文件名对应关系
     # 用key批量解密当前文件夹下面所有文件
     for i, n in enumerate(os.listdir(path)):
@@ -155,10 +125,10 @@ def batch_debiter(key='Archer', path='.', rec=False):
             continue
         if n == '0.sxf':
             with open(fd, 'rb') as f:
-                buff = byte_lock(f.read())
-                names = json.loads(buff)
+                buff = bytes(map(lambda x: x ^ 255, f.read()))
+                names = json.loads(buff.decode())
             for k, v in names.items():
-                if debiter(os.path.join(path, k), key):
+                if ezbiter(os.path.join(path, k), key):
                     os.rename(os.path.join(path, k), os.path.join(path, v))
                 else:
                     return False
